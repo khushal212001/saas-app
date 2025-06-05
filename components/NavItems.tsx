1910d93790296372b92556319e0392aaa1b07c3b
Khushal <khushalgarg85886@gmail.com>
2025-06-05 19:30:23 +0530

    fixed navbar

--- components/NavItems.tsx
+++ components/NavItems.tsx
@@ -1,39 +1,31 @@
-"use client"
-import Link from "next/link"
-import { usePathname } from "next/navigation"
-import { cn } from "@/lib/utils"
-const navItems=[
-  {
-    label:"Home",
-    href:'/'
-  },
-  {
-    label:"Companions",
-    href:"/companions"
-  },
-  {
-    label:"My Journey",
-    href:"my-journey"
-  }
+'use client';
+
+import Link from "next/link";
+import {usePathname} from "next/navigation";
+import {cn} from "@/lib/utils";
+
+const navItems = [
+    { label:'Home', href: '/' },
+    { label: 'Companions', href: '/companions' },
+    { label: 'My Journey', href: 'my-journey' },
 ]
+
 const NavItems = () => {
+    const pathname = usePathname();
 
-  const pathname= usePathname()
-
-  return (
-    <nav className="flex items-center">
-      {
-        navItems.map(({label,href})=>(
-          <Link 
-            href={href} 
-            key={label} 
-            className={cn(pathname===href && 'text-primary font-semibold gap-2 mr-2.5 ml-2.5')}> 
-              {label} <br/>
-          </Link>
-        ))
-      }
-    </nav>
-  )
+    return (
+        <nav className="flex items-center gap-4">
+            {navItems.map(({ label, href }) => (
+                <Link
+                    href={href}
+                    key={label}
+                    className={cn(pathname === href && 'text-primary font-semibold gap-2 mr-2.5')}
+                >
+                    {label+ "  "}
+                </Link>
+            ))}
+        </nav>
+    )
 }
 
 export default NavItems
